f81783e1873b37eab42fb724d1a66ba4254ae055
baouragh <baouragh@e1r5p4.1337.ma>
2023-10-31 23:17:29 +0100

    all

---
+++ libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baouragh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 15:36:57 by baouragh          #+#    #+#             */
+/*   Updated: 2023/10/31 15:47:09 by baouragh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include<unistd.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+
+#endif
