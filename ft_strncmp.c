a0f7db657d2054f1e6be62917f38d2b699168e6a
baouragh <baouragh@e1r5p3.1337.ma>
2023-11-01 12:46:48 +0100

    do it

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,27 +6,20 @@
 /*   By: baouragh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 11:11:27 by baouragh          #+#    #+#             */
-/*   Updated: 2023/11/01 11:11:38 by baouragh         ###   ########.fr       */
+/*   Updated: 2023/11/01 12:46:36 by baouragh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include<stdio.h>
-#include<string.h>
+#include"libft.h"
 
-int ft_strncmp(const char *s1, const char *s2 , size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while(n - 1 > 0 && s1[i] - s2[i] == 0)
-    {
-        i++;
-        n--;
-    }
-    return(s1[i] - s2[i]);
-}
-int main()
-{
-    char *d1 = "hells";
-    char *d2 = "helll";
-    printf("%d\n org : %d", ft_strncmp(d1, d2, 5), strncmp(d1, d2, 5));
+	i = 0;
+	while (n - 1 > 0 && s1[i] - s2[i] == 0)
+	{
+		i++;
+		n--;
+	}
+	return (s1[i] - s2[i]);
 }
