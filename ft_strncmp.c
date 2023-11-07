df81f7b7f04e139bf2aaf944a884a2c9cb878fe0
baouragh <baouragh@e1r5p4.1337.ma>
2023-11-07 17:36:51 +0100

    do it

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -16,7 +16,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	int	i;
 
 	i = 0;
-	while (n - 1 > 0 && s1[i] - s2[i] == 0)
+	while (n - 1 > 0 && s1[i] - s2[i] == 0) // TODO: ceck n
 	{
 		i++;
 		n--;
--- ft_substr.c
+++ ft_substr.c
@@ -11,27 +11,8 @@
 /* ************************************************************************** */
 
 #include"libft.h"
+
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
-    char *to_start;
-    char *to_last;
-    size_t total;
-    size_t rest;
-    size_t removed;
-
-    to_start = (char*)s;
-    while(start-- > 0)
-    to_start++;
-    total = ft_strlen(to_start);
-    to_last = to_start + len;
-    rest = ft_strlen(to_last);
-    removed = total - rest;
-    ft_bzero(to_last,removed);
-    return(ft_strdup(to_start));
-}
-int main()
-{
-  char *str = "Hello World";
-   char *copy = ft_substr(str,6,5);
-   printf("copy:%s",copy);
+  
 }
