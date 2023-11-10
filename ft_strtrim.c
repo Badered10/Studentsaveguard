b90698237c78e4ad4d7c5e6be4aa01a56545514b
baouragh <baouragh@e1r13p7.1337.ma>
2023-11-10 21:16:29 +0100

    do it

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,87 +6,89 @@
 /*   By: baouragh <baouragh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 21:29:16 by baouragh          #+#    #+#             */
-/*   Updated: 2023/11/10 17:11:39 by baouragh         ###   ########.fr       */
+/*   Updated: 2023/11/10 21:16:15 by baouragh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
-
-
-
-// string = ABCDEFGH;
-// find = GH;
-
-
-
-
-
-
-
-
 #include "libft.h"
 
-static char  *ft_strrnstr(const char *first, const char *set , size_t len)
+static int ft_same(char s, char c)
 {
-  char *ocean;
-  char *fish;
-  int i ;
-  i = 0;
+		return((s == c));
+}
+static size_t first_wall(const char *s1,const char *set)
+{
+	int wall;
+	char *chars;
+	char *string;
 	
-	ocean = (char *)first + ft_strlen(first);
-  if(!*first && !*set)
-    return((char *)first);
-	if(!*set)
-	return(ocean);
-	if(!*first)
-	return NULL;
-    ocean = (char *)first + ft_strlen(first) - 1;
-    fish = (char *)set + ft_strlen(set) - 1;
-    while (len-- && (ocean - first != 0))
-    {
-        while ((fish - set != 0 ) && (*ocean == *fish))
-        {
-            ocean--;
-            fish--;
-            i++;
-        }
-        if (fish - set == 0)
-            return (ocean);
-        ocean += i;
-        i = 0;
-        ocean--;
-        fish =(char *)set + ft_strlen(set) - 1;
-    }
-    return NULL;
+	wall = 0;
+	string = (char*)s1;
+	chars = (char*)set;
+	while(*string)
+	{
+		if(ft_same(*string,*chars) == 1 && *string)
+		{
+			string++;
+			wall++;
+		}
+			chars = (char *)set;
+			while(ft_same(*string,*chars) != 1 && *chars)
+			chars++;
+			if(!*chars)
+			break;
+	}
+	return (wall);
+}
+static size_t last_wall(const char *s1,const char *set,size_t lenth)
+{
+	char *string;
+	char *chars;
+	int wall;
+	
+	wall = 0;
+	string = (char*)s1 + lenth - 1;
+	chars = (char*)set;
+	while (lenth)
+	{
+		if(ft_same(*string,*chars) == 1 && lenth)
+			{
+				lenth--;
+				string--;
+				wall++;
+			}
+			chars = (char *)set;
+			while(ft_same(*string,*chars) != 1 && *chars)
+			chars++;
+			if(!*chars)
+			break;
+	}
+	return (wall);
 }
 
 char *ft_strtrim(char const *s1, char const *set)
 {
-    char *first;
-    char	*end;
-	char *trim ;
-	int diff ;
+	char *trim;
+	size_t first;
+	size_t last;
+	size_t lenth;
+	size_t diff;
+	size_t total;
 	
-	
-    first  = ft_strnstr(s1,set,ft_strlen(s1));
-	first += ft_strlen(set);
-	end = ft_strrnstr(s1, set , ft_strlen(first));
-	if((end && first) && first != end)
-	diff = end - first ;
-	if(end == first)
-	diff = ft_strlen(first);
-	if(!end || !first)
-	return NULL;
-	trim = (char*)ft_calloc(diff + 1 ,sizeof(char));
-	if(!trim)
-	return(NULL);
-	ft_memcpy(trim, first , diff);
+
+	lenth = ft_strlen(s1);
+	first = first_wall(s1,set);
+	last = last_wall(s1,set,lenth);
+	diff = first + last;
+	total = (lenth - diff) + 1;
+	trim = (char*)malloc(sizeof(char) * total);
+	ft_bzero(trim, total);
+	ft_memcpy(trim,s1 + first, total - 1);
 	return(trim);
 }
-int main() {
-  char *b = ft_strtrim("   xxxtripouille x  x  xdasdasd", " x");
-  printf("%s", b);
-  free(b);
-  return 0;
-}
+// int main() {
+//   char *b = ft_strtrim("      xxxxxtripouille x  x  xdasdasd sdcccxxxxx      ", " x");
+//   printf("%s", b);
+//   free(b);
+//   return 0;
+// }
