c31214acfffffa5bfbdc9c403afcbdfd307980ab
baouragh <baouragh@e1r9p4.1337.ma>
2023-11-12 09:30:15 +0100

    do it

--- ft_split.c
+++ ft_split.c
@@ -6,35 +6,27 @@
 /*   By: baouragh <baouragh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 13:18:29 by baouragh          #+#    #+#             */
-/*   Updated: 2023/11/11 22:19:17 by baouragh         ###   ########.fr       */
+/*   Updated: 2023/11/12 09:29:57 by baouragh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
-static char	**ft_strdupp(const char *s1)
+static char	**ft_strdupp(char **res)
 {
-	char	*copy;
-	size_t	lenth;
-    char    **res;
-
-    res = (char**)malloc(sizeof(char*) * (words + 1));
+    res = (char**)malloc(sizeof(char*) *  1);
     if(!res)
     return NULL;
-	lenth = ft_strlen(s1);
-	copy = (char *)malloc(lenth + 1);
-	if (!copy)
-		return (NULL);
-	ft_strlcpy(copy, s1, lenth + 1);
-	return (copy);
+    *res = ft_strdup("");
+	return (res);
 }
 
 static int ft_wcounter(const char *s, char c)
 {
     char *x;
-    int i;
+    int words;
 
-    i = 0;
+    words = 0;
     x = (char*)s;
     while(*x)
     {
@@ -44,12 +36,10 @@ static int ft_wcounter(const char *s, char c)
         {
             while(*x != c && *x)
             x++;
-            i++;            
+            words++;            
         }
-        while(*x == c)
-        x++;
     }
-    return (i);
+    return (words);
 }
 
 char **ft_split(char const *s, char c)
@@ -58,10 +48,12 @@ char **ft_split(char const *s, char c)
     char *p;
     char *start;
     int words;
+    int to_free;
     
+    to_free = 1;
     words = ft_wcounter(s, c);
     if(!words)
-    return (*res = strdup(""), res);
+    return (ft_strdupp(res));
     res = (char**)malloc(sizeof(char*) * (words + 1));
     if(!res)
     return NULL;
@@ -76,8 +68,10 @@ char **ft_split(char const *s, char c)
         *res = ft_substr(start,0,p - start);
         if(!*res)
         {
-            while(words--)
-            free(res[words]);
+            to_free = 1;
+            to_free = ft_wcounter(s, c) - words; 
+            while(to_free-- >= 0)
+            free(res[to_free]);
             free(res);
             return NULL;
         }
@@ -89,14 +83,17 @@ char **ft_split(char const *s, char c)
 }
 int main ()
 {
-    const char *s ="         ";
+    const char *s =" cx  asd dsa";
     char c = ' ';
-    // char **k = ft_split(s, c);
-    // int i = 0;
-    printf("%d",ft_wcounter(s, c));
-    //  while(i < ft_wcounter(s, c))
-    //  {
-    //     printf("%s\n",k[i]);
-    //     i++;
-    //  }
+    char **k = ft_split(s, c);
+    int i = 0;
+    int words;
+    words = ft_wcounter(s, c);
+    if(words == 0)
+    words = 1;
+     while(i < words)
+     {
+        printf("%s\n",k[i]);
+        i++;
+     }
 }
