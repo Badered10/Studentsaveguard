0529a8920178a24c4328959e3c521b98c3299107
baouragh <baouragh@e1r6p4.1337.ma>
2023-11-16 11:49:23 +0100

    do it

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,34 @@
 /*   By: baouragh <baouragh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 11:41:50 by baouragh          #+#    #+#             */
-/*   Updated: 2023/11/16 11:41:51 by baouragh         ###   ########.fr       */
+/*   Updated: 2023/11/16 11:49:01 by baouragh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include"libft.h"
+
+t_list *ft_lstlast(t_list *lst)
+{
+    t_list *tmp;
+    
+    if(!lst)
+    return NULL;
+    while(lst)
+    {
+        tmp = lst;
+        lst = lst->next;
+    }
+    return(tmp);
+}
+
+int main(void)
+{
+    t_list *head;
+    t_list *ok;
+    int x;
+
+    head = ft_lstnew("hello");
+    ok = ft_lstlast(head);
+    x = ft_lstsize(head);
+    printf("%s and size is :%d",(char*)ok->content,x);
+}
