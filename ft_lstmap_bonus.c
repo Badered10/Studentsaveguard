6fc87e64283fa166ab285073d8da79c99f866f73
baouragh <baouragh@e1r6p5.1337.ma>
2023-11-17 20:38:20 +0100

    do it

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: baouragh <baouragh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 18:52:33 by baouragh          #+#    #+#             */
-/*   Updated: 2023/11/17 19:58:28 by baouragh         ###   ########.fr       */
+/*   Updated: 2023/11/17 20:32:40 by baouragh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,26 @@ t_list *ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
 {
     if (!lst || !f || !del)
     return (NULL);
-
-    t_list *tmp;
-    while(lst)
+    void *new_content;
+    t_list *new_list;
+    t_list *new_node;
+    (lst)
     {
-        tmp = f(lst);
-        if (!tmp)
+        new_content = f(lst->content);
+        if (!new_content)
         {
-            del(tmp->content)
-            ft_lstclear(tmp);
+            ft_lst_clear(&new_list ,new_content);
+            return(NULL);
         }
-        ft_lstadd_back(tmp);
+        new_node = ft_lstnew(new_content);
+        if (!new_node)
+        {
+            del(new_content);
+            ft_lstclear(&new_list,new_node);
+            return (NULL);
+        }
+        t_lstadd_back(&new_list,new_node);
         lst = lst->next;
     }
+    return(new_list);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: baouragh <baouragh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 15:36:57 by baouragh          #+#    #+#             */
-/*   Updated: 2023/11/17 19:59:01 by baouragh         ###   ########.fr       */
+/*   Updated: 2023/11/17 20:37:15 by baouragh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,7 @@ size_t				ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t				ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t				ft_strlen(const char *s);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
-char				*ft_strnstr(const char *haystack, const char *needle,
-						size_t len);
+char				*ft_strnstr(const char *haystack, const char *needle,size_t len);
 char				*ft_strrchr(const char *s, int c);
 char				*ft_strtrim(char const *s1, char const *set);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
