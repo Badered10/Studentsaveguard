e955fa26acca5f3b517758c0065007722d14b06c
baouragh <baouragh@e1r10p5.1337.ma>
2023-11-22 19:08:27 +0100

    PROGRESSSS in GET NEXT LINE git add .git add .

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,138 +6,11 @@
 /*   By: baouragh <baouragh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 11:38:18 by baouragh          #+#    #+#             */
-/*   Updated: 2023/11/22 13:39:28 by baouragh         ###   ########.fr       */
+/*   Updated: 2023/11/22 19:04:51 by baouragh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-size_t	ft_strlen(const char *s)
-{
-	char	*tmp;
-
-	tmp = (char *)s;
-	while (*s != '\n' || *s != '\0')
-		;
-    if (!*s)
-	s -= 1;
-	return (s - tmp);
-}
-
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	char	*source;
-	size_t	source_lenth;
-
-    if (!dst || !src)
-        return(NULL);
-
-	source = (char *)src;
-	source_lenth = ft_strlen(source);
-	if (dstsize == 0)
-		return (source_lenth);
-	dstsize -= 1;
-	while ((dstsize > 0) && *source)
-	{
-		*dst++ = *source++;
-		if (dstsize != 0)
-			dstsize--;
-	}
-	*dst = '\0';
-	return (source_lenth);
-}
-
-
-char *alloc_for_me(t_liat *list)
-{
-    char *final;
-    size_t i;
-    i  = 0;
-    t_list *tmp;
-    size_t ok;
-    ok  = 0;
-    tmp = list;
-    while(tmp)
-    {
-        i += ft_strlen(tmp->text);
-        tmp = tmp->next;
-    }
-    final = malloc(sizeof(char) * i);
-    while(list)
-    {
-        ft_strlcpy(final + ok,list->text,ft_strlen(list->text))
-        ok += ft_strlen(list->text);
-        list = list->next;
-    }
-    final[ok] = '\0';
-    return(final);
-    
-}
-
-t_list *last(t_list *node)
-{
-    t_list *res;
-    if (!node)
-    return(NULL);
-    while(node)
-    {
-        node = res;
-        node = node->next;
-    }
-    return (res);
-}
-
-void add_back(t_list **list, t_list *new)
-{
-    t_list *tmp;
-    if (!list || !new)
-    return;
-    if (!*lst)
-    {
-        *lst = new;
-        return;
-    }
-    tmp = last(*lst);
-    tmp->next = new;
-}
-
-t_list *new_node(char *s,size_t byets_readed)
-{
-    t_list *new;
-    if (!s)
-    return NULL;
-    new = malloc(sizeof(t_list));
-    if (!new)
-    return NULL;
-    new->text = malloc(ft_strlen(s) + 1);
-    if (!new->text)
-    return NULL;
-    ft_strlcpy(new->next,s,ft_strlen(s) + 1)
-    new->next = NULL;
-    return (new);
-}
-
-t_list *my_list(t_list **list,int fd)
-{
-    char buffer[BUFFER_SIZE];
-    size_t byets_readed;
-    t_list *tmp;
-    t_list *rest;
-    size_t count;
-    
-    while (!n_finder((*list)->text))
-    {
-        byets_readed = read(fd,buffer,BUFFER_SIZE);
-        if (!byets_readed)
-        return;
-        *list = new_node(buffer,byets_readed);
-        add_back(*list);
-    }
-    tmp = last(*list);
-    ft_strlcpy()                                                                                                                                                                                                                                                           
-    
-}
 
 int n_finder(char *s)
 {
@@ -154,6 +27,160 @@ int n_finder(char *s)
     return (0);
 }
 
+size_t	ft_strlen(const char *s)
+{
+	int i;
+ 
+    i = 0;
+	while (s[i] != '\n' && s[i] != '\0')
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	char	*source;
+	size_t	source_lenth;
+    int i;
+    
+    i = 0;
+	source = (char *)src;
+    while(source[i] != '\n' && source[i] != '\0')
+    i++;
+	source_lenth = i;
+    
+	if (dstsize == 0)
+		return (source_lenth);
+	dstsize -= 1;
+	while ((dstsize > 0) && *source)
+	{
+		*dst++ = *source++;
+		if (dstsize != 0)
+			dstsize--;
+	}
+	*dst = '\0';
+	return (source_lenth);
+}
+
+t_list *new_node(char *s)
+{
+    t_list *new;
+    if (!s)
+    return NULL;
+    new = malloc(sizeof(t_list));
+    if (!new)
+    return NULL;
+    new->text = malloc(ft_strlen(s) + 1);
+    if (!new->text)
+    return NULL;
+    ft_strlcpy(new->text ,s,ft_strlen(s) + 1);
+    new->next = NULL;
+    return (new);
+}
+
+t_list *ft_last(t_list *node)
+{
+    t_list *res;
+    if (!node)
+    return(NULL);
+    while(node)
+    {
+        res = node;
+        node = node->next;
+    }
+    return (res);
+}
+
+void ft_clean(t_list **list)
+{
+    t_list *tmp;
+    char *iter;
+    char *str;
+    if (!list)
+    return ;
+
+    int i = 0;
+    int n = 0;
+    tmp = ft_last((*list));
+    iter = tmp->text;
+    while (iter[i] != '\n' && iter[i] != '\0')
+        i++;
+    while(iter[i + n] != '\0')
+        n++;
+    str = malloc(sizeof(char) * n + 1);
+    strlcpy(str, iter + i , n + 1);
+    free(*list);
+    (*list) = new_node(str);
+}
+
+void add_back(t_list **list, t_list *new)
+{
+    t_list *tmp;
+    if (!list || !new)
+    return;
+    if (!*list)
+    {
+        *list = new;
+        return;
+    }
+    tmp = ft_last(*list);
+    tmp->next = new;
+}
+
+char *alloc_for_me(t_list *list)
+{
+    char *final;
+    size_t i;
+    i  = 0;
+    t_list *tmp;
+    size_t ok;
+    ok  = 0;
+    tmp = list;
+    while(tmp)
+    {
+        i += ft_strlen(tmp->text);
+        tmp = tmp->next;
+    }
+    final = malloc(sizeof(char) * i);
+    while(list)
+    {
+        ft_strlcpy(final + ok,list->text,ft_strlen(list->text));
+        ok += ft_strlen(list->text);
+        list = list->next;
+    }
+    final[ok - 1] = '\0';
+    return(final);
+    
+}
+
+void my_list(t_list **list,int fd,size_t size)
+{
+    char buffer[BUFFER_SIZE];
+    size_t byets_readed;
+    t_list *new;
+    
+    if (!*list)
+    {
+        byets_readed = read(fd,buffer,BUFFER_SIZE);
+        if (!byets_readed)
+        return;
+        *list = new_node(buffer);
+        if(*list)
+        return;
+    }
+
+    while(!n_finder((*list)->text))
+    {
+        byets_readed = read(fd,buffer,BUFFER_SIZE);
+        if (!byets_readed)
+        return;
+        new = new_node(buffer);
+        if(*list)
+        return;
+        add_back(&(*list), new);
+    }
+}
+
 char *get_next_line(int fd)
 {
     static t_list *list;
@@ -163,15 +190,21 @@ char *get_next_line(int fd)
         
     if (fd < 0 || BUFFER_SIZE <= 0 || read(fd,&next_line,0) < 0)
     return NULL;
-    my_list(&list,fd);
-   next_line = alloc_for_me(list);
-   
-    
+    my_list(&list,fd,BUFFER_SIZE);
+    if(list)
+    next_line = alloc_for_me(list);
+   ft_clean(&list);
+   return (next_line);
 }
+
 int main()
 {
     int fd = open("test.txt", O_CREAT | O_RDWR, 0777);
     char *str;
+    str = get_next_line(fd);
+    printf("%s\n",str);
+    str = get_next_line(fd);
+    printf("%s\n",str);
     str = get_next_line(fd);
     printf("%s\n",str);
     close(fd);
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,78 +6,27 @@
 /*   By: baouragh <baouragh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 16:26:03 by baouragh          #+#    #+#             */
-/*   Updated: 2023/11/21 13:54:20 by baouragh         ###   ########.fr       */
+/*   Updated: 2023/11/22 18:47:10 by baouragh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "get_next_line.h"
 
-void	*ft_memset(void *b, int c, size_t len)
-{
-	char	*temp;
-	size_t	i;
+// size_t	ft_strlen(const char *s)
+// {
+// 	char	*tmp;
+    
+// 	tmp = (char *)s;
+// 	while (*s != '\n' && *s != '\0')
+// 		s++;
+//     if (!*s)
+// 	s -= 1;
+// 	return (s - tmp);
+// }
 
-	temp = (char *)b;
-	i = 0;
-	while (i < len)
-		temp[i++] = c;
-	return (b);
-}
-void	ft_bzero(void *b, size_t n)
-{
-	ft_memset(b, 0, n);
-}
-
-void	*ft_calloc(size_t count, size_t size)
-{
-	size_t	total_bytes;
-	void	*ptr;
-
-	if (count && (SIZE_MAX / count < size))
-		return (NULL);
-	total_bytes = count * size;
-	ptr = (void *)malloc(total_bytes);
-	if (!ptr)
-		return (NULL);
-	ft_bzero(ptr, total_bytes);
-	return (ptr);
-}
-
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	unsigned char	*source;
-	unsigned char	*destination;
-
-	source = (unsigned char *)src;
-	destination = (unsigned char *)dst;
-	if (!destination && !source)
-		return (NULL);
-	if ((destination == source) && n)
-		return (dst);
-	while (n--)
-		*destination++ = *source++;
-	return (dst);
-}
-
-void *ft_memmove(void *dst, const void *src, size_t len)
-{
-	unsigned char	*destination;
-	unsigned char	*source;
-
-	if (!src && !dst)
-		return (NULL);
-	if (src > dst)
-	{
-		ft_memcpy(dst, src, len);
-		return (dst);
-	}
-	destination = (unsigned char *)dst;
-	source = (unsigned char *)src;
-	while (len != 0)
-	{
-		destination[len - 1] = source[len - 1];
-		len--;
-	}
-	return ((void *)destination);
-}
+// int main ()
+// {
+// 	char s[] = "hello world\n my name is bader";
+// 	printf("%zu",ft_strlen(s));
+// }
