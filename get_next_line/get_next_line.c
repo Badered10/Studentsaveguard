b80cd23678747a69d59e1f20d248c7e84b706833
baouragh <baouragh@e1r6p4.1337.ma>
2023-11-30 15:56:28 +0100

    get next line part 1 handel BUFFER SIZE

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: baouragh <baouragh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 18:43:49 by baouragh          #+#    #+#             */
-/*   Updated: 2023/11/30 10:03:06 by baouragh         ###   ########.fr       */
+/*   Updated: 2023/11/30 15:55:53 by baouragh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,39 +83,43 @@ char	*get_store(int fd, char *store, char *res, char *buffer)
 	int	check;
 
 	check = 1;
-	if (fd < 0 || read(fd, buffer, 0) < 0)
-		return (free(store), NULL);
+	if (fd < 0 || read(fd, buffer, 0) < 0 || BUFFER_SIZE <= 0)
+		return (free(store),free(buffer),NULL);
 	while (check > 0)
 	{
 		check = read(fd, buffer, BUFFER_SIZE);
 		if (check == 0 && ((!store) || (store[0] == '\0')))
-			return (free(store), NULL);
+			return (free(store),free(buffer),NULL);
 		if (check == -1)
-			return (free(store), NULL);
+			return (free(store),free(buffer),NULL);
 		buffer[check] = 0;
 		res = ft_strdup(store);
 		if (store)
 			free(store);
 		store = ft_strjoin(res, buffer);
 		if (store[0] == '\0' && res)
-			return (free(res), free(store), (NULL));
+			return (free(res), free(store),free(buffer),(NULL));
 		if (res)
 			free(res);
 		if (ft_strchr(store, '\n') != NULL)
 			break ;
 	}
+	free(buffer);
 	return (store);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*store;
-	char		buffer[BUFFER_SIZE + 1];
+	char		*buffer;
 	char		*res;
 	int			check;
 
 	res = NULL;
 	check = 0;
+	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if(!buffer)
+		return(free(store),NULL);
 	store = get_store(fd, store, res, buffer);
 	if (!store)
 		return (NULL);
@@ -131,26 +135,14 @@ char	*get_next_line(int fd)
 	return (res);
 }
 
-// #include <limits.h>
-// OPEN_MAX
-// int main()
-// {
-// 	char *res;
-// 	int fd = open("test.txt",O_CREAT | O_RDWR , 0777);
-
-// 	res = get_next_line(fd);
-// 	printf("%s",res);
-// 	free(res);
-
-// 	fd = open("2.txt",O_CREAT | O_RDWR , 0777);
-// 	res = get_next_line(fd);
-// 	printf("%s",res);
-// 	free(res);
-
-// 	fd = open("test.txt",O_CREAT | O_RDWR , 0777);
-
-// 	res = get_next_line(fd);
-// 	printf("%s",res);
-// 	free(res);
-// 	system("leaks a.out");
-// }
+int main()
+{
+	char *res;
+	int fd1;
+	
+	fd1 = open("2.txt", O_RDONLY , 0777);
+	res = get_next_line(fd1);
+	printf("%s",res);
+	res = get_next_line(fd1);
+	printf("%s",res);
+}
