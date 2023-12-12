c4c18f7fe46092e022324bf23c1e997ca62fa071
baouragh <baouragh@e1r3p5.1337.ma>
2023-12-12 21:08:03 +0100

    never forget STATIC var is always intalized by 0 as default

--- ft_printf/ft_cflags.c
+++ ft_printf/ft_cflags.c
@@ -6,7 +6,7 @@
 /*   By: baouragh <baouragh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 19:57:29 by baouragh          #+#    #+#             */
-/*   Updated: 2023/12/12 20:29:20 by baouragh         ###   ########.fr       */
+/*   Updated: 2023/12/12 21:06:24 by baouragh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 int ft_cflags(char *string, int x)
 {
-    f nes;
-    static int y = 1;
-
-    nes.spaces = 0;
-    nes.count = 0;
+    static f nes;
     while(*string == '-' && *(string + 1))
+    {
+        nes.mince += 1;
         string++;
+    }
     nes.d = x;
     nes.len = 1;
     if(ft_isdigit(*string))
@@ -35,9 +34,10 @@ int ft_cflags(char *string, int x)
        nes.spaces = 0;
     while(ft_isdigit(*string))
         string++;
-    while (((nes.spaces)-- > 0) && y!= 1)
+    while (((nes.spaces)-- > 0) && (nes.mince != 1))
+    {
         nes.count += write(1," ",1);
+    }
         nes.count += ft_putchar_fd(nes.d,1);
-    y++;
         return (nes.count);
 }
